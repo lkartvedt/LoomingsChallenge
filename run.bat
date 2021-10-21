8e8da58f8f1f62135f5beb92cf3ef1113ce7f6da
Lindsey <lindseykartvedt@gmail.com>
2021-10-21 02:29:24 -0700

    Scripts for windows and bash. Also, compiled artifacts to run.

---
+++ run.bat
@@ -0,0 +1,2 @@
+java -jar .\out\artifacts\LoomingsChallenge_jar\LoomingsChallenge.jar
+pause
---
+++ run.sh
@@ -0,0 +1,3 @@
+# For some reason, java could not open the jar file when I directly ran it without echo
+echo -e "$(java -jar "out/artifacts/LoomingsChallenge_jar/LoomingsChallenge.jar")"
+read -p "Press any key to continue . . ."
